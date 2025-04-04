8b842e32eb3ebd740eddd90fe487bdd5ea635e2d
oden73 <karpuk.max1@gmail.com>
2025-04-04 13:38:27 +0300

    fastapi app set up + endpoints set

--- main.py
+++ main.py
@@ -3,3 +3,30 @@ The entry point of FastAPI application.
 This file defines API routes and orchestrates
 the different components of the system
 """
+
+from fastapi import FastAPI, File, UploadFile, HTTPException
+import os
+import uuid
+import logging
+import shutil
+
+
+logging.basicConfig(filename='app.log', level=logging.INFO)
+
+app = FastAPI()
+
+
+def chat():
+    pass
+
+
+def upload_and_index_document():
+    pass
+
+
+def list_documents():
+    pass
+
+
+def delete_document():
+    pass
