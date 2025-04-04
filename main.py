cd6e611aba342cbe0b3e8c7b7010315a53650a73
oden73 <karpuk.max1@gmail.com>
2025-04-04 16:29:19 +0300

    fast api changes

--- main.py
+++ main.py
@@ -4,29 +4,37 @@ This file defines API routes and orchestrates
 the different components of the system
 """
 
+from model.pydantic_models import QueryInput, QueryResponse, DocumentInfo, DeleteFileRequest
+
 from fastapi import FastAPI, File, UploadFile, HTTPException
 import os
 import uuid
 import logging
 import shutil
-
+from typing import List
 
 logging.basicConfig(filename='app.log', level=logging.INFO)
 
-app = FastAPI()
 
+class App:
+    def __init__(self) -> None:
+        self.app: FastAPI = FastAPI()
 
-def chat():
-    pass
+        @self.app.post('/chat', response_model=QueryResponse)
+        def chat(query_input: QueryInput) -> QueryResponse:
+            pass
 
+        @self.app.post('/upload-doc')
+        def upload_and_index_document(file: UploadFile = File(...)):
+            pass
 
-def upload_and_index_document():
-    pass
+        @self.app.get('/list-docs', response_model=List[DocumentInfo])
+        def list_documents() -> List[DocumentInfo]:
+            pass
 
+        @self.app.post('/delete-doc')
+        def delete_document(request: DeleteFileRequest):
+            pass
 
-def list_documents():
-    pass
-
-
-def delete_document():
-    pass
+    def run(self) -> None:
+        pass
